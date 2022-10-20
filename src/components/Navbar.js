5cfaa96b56f2cd17c8d00f996b0d5e273e3b0870
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-10-20 22:35:54 +0200

    working on context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Navbar, Modal } from "react-bootstrap";
 
 const NavBarComponent = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Navbar expand="sm">
         <Navbar.Brand href="/">Ecommerce Shop</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button>Cart 0 Items</Button>
+          <Button onClick={handleShow}>Cart 0 Items</Button>
         </Navbar.Collapse>
       </Navbar>
-      <Modal show={show} onHide={handleClose} />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>ShoppingCart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>This is the modal body</h1>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
